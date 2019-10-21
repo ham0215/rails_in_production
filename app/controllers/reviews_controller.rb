b70570dcfa9fe6e438b21e49bfbb89b90328d0bb
hamadanaoto <teiru_3@yahoo.co.jp>
2019-10-22 02:38:23 +0900

    Add reviews index action

---
+++ app/controllers/reviews_controller.rb
@@ -0,0 +1,11 @@
+class ReviewsController < ApplicationController
+  def index
+    render json: Review.where(status: Review::statuses[index_params[:status]])
+  end
+
+  private
+
+  def index_params
+    params.require(:reviews).permit(:user_id, :status)
+  end
+end
