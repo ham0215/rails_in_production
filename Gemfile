c68655a9bb5094e64d243abc2bc463fe5b742a62
naoto hamada <hamada@vis-its.com>
2019-10-24 15:26:07 +0900

    Add memory_profiler

--- Gemfile
+++ Gemfile
@@ -22,6 +22,8 @@ gem 'jbuilder', '~> 2.7'
 
 gem 'activerecord-import'
 
+gem 'memory_profiler'
+
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
--- app/models/user.rb
+++ app/models/user.rb
@@ -25,7 +25,6 @@ class User < ApplicationRecord
 
         transaction do
           r.report 'insert_all' do
-            #bulk_insert_using_insert_all(import_data.map(&:attributes))
             100.times { bulk_insert_using_insert_all(insert_data) }
           end
           raise ActiveRecord::Rollback
@@ -37,6 +36,46 @@ class User < ApplicationRecord
           end
           raise ActiveRecord::Rollback
         end
+      end
+    end
+
+    def profiler_bulk_insert
+      # create data
+      import_data = []
+      1_000.times { import_data << new(name: 'name', created_at: Time.current, updated_at: Time.current) }
+
+      insert_data = []
+      1_000.times { insert_data << { name: 'name', created_at: Time.current, updated_at: Time.current } }
+
+      values = []
+      1_000.times { values << "('name', '#{Time.current.to_s(:db)}', '#{Time.current.to_s(:db)}')" }
+      sql = "INSERT INTO users (name, created_at, updated_at) VALUES #{values.join(',')}"
+
+      p '################# sql ########################'
+      transaction do
+        report = MemoryProfiler.report do
+          bulk_insert_using_sql(sql)
+        end
+        report.pretty_print(retained_strings: 0, allocated_strings: 100, normalize_paths: true)
+        raise ActiveRecord::Rollback
+      end
+
+      p '################# insert_all ########################'
+      transaction do
+        report = MemoryProfiler.report do
+          bulk_insert_using_insert_all(insert_data)
+        end
+        report.pretty_print(retained_strings: 0, allocated_strings: 100, normalize_paths: true)
+        raise ActiveRecord::Rollback
+      end
+
+      p '################# import ########################'
+      transaction do
+        report = MemoryProfiler.report do
+          bulk_insert_using_import(import_data)
+        end
+        report.pretty_print(retained_strings: 0, allocated_strings: 100, normalize_paths: true)
+        raise ActiveRecord::Rollback
       end
     end
 
