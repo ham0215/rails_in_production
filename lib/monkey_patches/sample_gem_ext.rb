2a16ea1c249ac0882860c569fb02ddcb9cbfea6b
naoto hamada <hamada@vis-its.com>
2020-04-16 16:23:21 +0900

    Add monkey patche

---
+++ lib/monkey_patches/sample_gem_ext.rb
@@ -0,0 +1,9 @@
+require 'sample_gem/version'
+unless SampleGem::VERSION == "0.1.0"
+  raise "Consider removing this patch"
+end
+
+module SampleGemMonkeyPatch
+end
+
+SampleGem.prepend(SampleGemMonkeyPatch)
