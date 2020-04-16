36e7f5e97ef11a20990878e3de9c30d0f679f0f1
naoto hamada <hamada@vis-its.com>
2020-04-16 16:51:08 +0900

    Add sample

--- lib/monkey_patches/sample_gem_ext.rb
+++ lib/monkey_patches/sample_gem_ext.rb
@@ -3,7 +3,24 @@ unless SampleGem::VERSION == '0.1.0'
   raise 'Consider removing this patch'
 end
 
-module SampleGemMonkeyPatch
-end
+module SampleKlassMonkeyPatch
+  def initialize(num)
+    raise SampleGem::NoIntegerError unless num == num.to_i
 
-SampleGem.prepend(SampleGemMonkeyPatch)
+    @num = num.to_i * 2
+  end
+
+  def increment
+    @num += 2
+  end
+end
+SampleGem::SampleKlass.prepend(SampleKlassMonkeyPatch)
+
+module SampleKlassClassMethodMonkeyPatch
+  def increment(num)
+    raise SampleGem::NoIntegerError unless num == num.to_i
+
+    num + 2
+  end
+end
+SampleGem::SampleKlass.singleton_class.prepend(SampleKlassClassMethodMonkeyPatch)
