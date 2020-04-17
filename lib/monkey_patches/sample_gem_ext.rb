77c5aa353cb6213ba3490e1c1c5af6062b3f0949
naoto hamada <hamada@vis-its.com>
2020-04-17 14:29:48 +0900

    Fix patch

--- lib/monkey_patches/sample_gem_ext.rb
+++ lib/monkey_patches/sample_gem_ext.rb
@@ -4,23 +4,16 @@ unless SampleGem::VERSION == '0.1.0'
 end
 
 module SampleKlassMonkeyPatch
-  def initialize(num)
-    raise SampleGem::NoIntegerError unless num == num.to_i
-
-    @num = num.to_i * 2
-  end
-
   def increment
-    @num += 2
+    Rails.logger.warn 'DEPRECATION WARNING: 使わないで！'
+    super
   end
 end
 SampleGem::SampleKlass.prepend(SampleKlassMonkeyPatch)
 
 module SampleKlassClassMethodMonkeyPatch
   def increment(num)
-    raise SampleGem::NoIntegerError unless num == num.to_i
-
-    num + 2
+    super(num.to_i)
   end
 end
 SampleGem::SampleKlass.singleton_class.prepend(SampleKlassClassMethodMonkeyPatch)
