823cb26fdd070c160509e3a7fe8fb9db49aa2c90
naoto hamada <hamada@vis-its.com>
2020-06-11 13:36:28 +0900

    Monkey patch (#11)

---
+++ lib/monkey_patches/sample_gem_ext.rb
@@ -0,0 +1,21 @@
+require 'sample_gem/version'
+unless SampleGem::VERSION == '0.1.0'
+  raise 'Consider removing this patch'
+end
+
+module SampleKlassMonkeyPatch
+  def increment
+    Rails.logger.warn 'DEPRECATION WARNING: 使わないで！'
+    super
+  end
+end
+SampleGem::SampleKlass.prepend(SampleKlassMonkeyPatch)
+
+module SampleKlassClassMethodMonkeyPatch
+  def increment(num)
+    super(num.to_i)
+  end
+end
+SampleGem::SampleKlass.singleton_class.prepend(SampleKlassClassMethodMonkeyPatch)
+
+SampleGem::SampleKlass::SAMPLE_CONST = 'hogehoge'.freeze
